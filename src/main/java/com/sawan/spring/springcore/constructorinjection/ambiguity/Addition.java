32d52d834bce4c870802fa927b4bcb9158c19dbb
Sawan Singh <sawansingh5216977@gmail.com>
2021-06-27 13:18:39 +0530

    constructor injection ambiguity and its solution

--- src/main/java/com/sawan/spring/springcore/constructorinjection/ambiguity/Addition.java
+++ src/main/java/com/sawan/spring/springcore/constructorinjection/ambiguity/Addition.java
@@ -2,16 +2,22 @@ package com.sawan.spring.springcore.constructorinjection.ambiguity;
 
 public class Addition {
 
-	public Addition(double a, double b) {
-		System.out.println("inside DOUBLE constructor");
-	}
+	/**
+	 * public Addition(double a, double b) { System.out.println("inside DOUBLE
+	 * constructor"); }
+	 * 
+	 * public Addition(int a, int b) { System.out.println("inside INT constructor");
+	 * }
+	 * 
+	 * public Addition(String a, String b) { System.out.println("inside STRING
+	 * constructor"); }
+	 * 
+	 */
 
-	public Addition(int a, int b) {
-		System.out.println("inside INT constructor");
-	}
-
-	public Addition(String a, String b) {
-		System.out.println("inside STRING constructor");
+	public Addition(int a, double b) {
+		System.out.println("inside constructor");
+		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
