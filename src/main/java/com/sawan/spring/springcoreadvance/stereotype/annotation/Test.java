da5a44def9bf0a8345b2ef75ef14a181a838ab1e
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-02 11:00:17 +0530

    implemented component & scope annotations

---
+++ src/main/java/com/sawan/spring/springcoreadvance/stereotype/annotation/Test.java
@@ -0,0 +1,20 @@
+package com.sawan.spring.springcoreadvance.stereotype.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/sawan/spring/springcoreadvance/stereotype/annotation/config.xml");
+		Instructor instructor = (Instructor) context.getBean("inst");
+		System.out.println(instructor.hashCode());
+
+		Instructor instructor2 = (Instructor) context.getBean("inst");
+		System.out.println(instructor2.hashCode());
+
+	}
+
+}
