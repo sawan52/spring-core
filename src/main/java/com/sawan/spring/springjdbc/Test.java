7a75044b7abe219a01dd5dcbc341c0c6aa76d4fd
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-03 14:23:56 +0530

    implemented JDBC thing using spring

---
+++ src/main/java/com/sawan/spring/springjdbc/Test.java
@@ -0,0 +1,20 @@
+package com.sawan.spring.springjdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/sawan/spring/springjdbc/config.xml");
+		JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
+
+		String sqlQuery = "insert into employee values(?,?,?)";
+		int result = jdbcTemplate.update(sqlQuery, new Integer(1), "Shivam", "Singh");
+		System.out.println("Number of records inserted are: " + result);
+
+	}
+
+}
