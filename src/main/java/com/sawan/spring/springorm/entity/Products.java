a2dc490b71a5457aa926c5cae3c2e1b193797f82
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-11 16:36:22 +0530

    first commit

---
+++ src/main/java/com/sawan/spring/springorm/entity/Products.java
@@ -0,0 +1,57 @@
+package com.sawan.spring.springorm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Products {
+
+	@Id
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "description")
+	private String desc;
+
+	@Column(name = "price")
+	private double price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
