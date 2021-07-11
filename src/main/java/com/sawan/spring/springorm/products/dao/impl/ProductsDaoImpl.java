ee09d017c95be754aac43cfa0663ccf7dd0bfce7
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-11 17:20:22 +0530

    added hibernate template and autowired it

--- src/main/java/com/sawan/spring/springorm/products/dao/impl/ProductsDaoImpl.java
+++ src/main/java/com/sawan/spring/springorm/products/dao/impl/ProductsDaoImpl.java
@@ -1,12 +1,16 @@
 package com.sawan.spring.springorm.products.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
 import com.sawan.spring.springorm.products.dao.ProductsDao;
 import com.sawan.spring.springorm.products.entity.Products;
 
+@Component
 public class ProductsDaoImpl implements ProductsDao {
 
+	@Autowired
 	HibernateTemplate hibernateTemplate;
 
 	@Override
