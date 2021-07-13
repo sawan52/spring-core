77cef39e9eb0fb4e8b6bb76dae53edeff2ca8603
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-13 23:08:06 +0530

    created SpringConfig.java file and configure the ViewResolver & DefaultServlet

---
+++ src/main/java/com/sawan/spring/springmvc/controller/SpringConfig.java
@@ -0,0 +1,29 @@
+package com.sawan.spring.springmvc.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ComponentScan("com.sawan.spring.springmvc.controller")
+@EnableWebMvc
+@Configuration
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
