b585b8a11336259d826199512edc4c70f13d3d9e
Sawan Singh <sawansingh5216977@gmail.com>
2021-07-13 13:32:57 +0530

    created 2 advices & add the pointcut expression

--- src/main/java/com/sawan/spring/springaop/aspects/LogginAspect.java
+++ src/main/java/com/sawan/spring/springaop/aspects/LogginAspect.java
@@ -1,8 +1,21 @@
 package com.sawan.spring.springaop.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LogginAspect {
+
+	@Before("execution(* com.sawan.spring.springaop.ProductServiceImpl.multiply(..))")
+	public void logBefore(JoinPoint joinPoint) {
+		System.out.println("Before executing method!");
+	}
+
+	@After("execution(* com.sawan.spring.springaop.ProductServiceImpl.multiply(..))")
+	public void logAfter(JoinPoint joinPoint) {
+		System.out.println("After executing method!");
+	}
 
 }
